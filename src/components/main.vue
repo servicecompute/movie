f4cb22757adbaad68badbe59210b8e6577d82fcf
stwie2k <269088185@qq.com>
2018-12-17 17:33:17 +0800

    页面打开自动加载电影

--- src/components/main.vue
+++ src/components/main.vue
@@ -58,14 +58,14 @@
         }],
         message:
         [
-{"id":1,"author":"曹雪芹","name":"红楼梦","price":32},
-{"id":2,"author":"施耐庵","name":"水浒传","price":30},
-{"id":"3","author":"罗贯中","name":"三国演义","price":24},
-{"id":4,"author":"吴承恩","name":"西游记","price":20}
 ],
+        movies:[],
          currentDate: new Date()
       }
     },
+     mounted: function() {
+        this.getMovies()
+     },
     methods: {
       choiceUrl (title) {
         this.title = title
@@ -74,9 +74,11 @@
 
         this.$router.push({path: '/detail', query: {search: this.content}})
       },
-      getCustomers: function() {
+      getMovies: function() {
           this.$http.get("http://localhost:9988/v1/buckets/movie_1/keys/name").then((response) => {
-                this.data.message=response.body;
+
+                  this.message=response.body
+
           },(response) => {
               console.log("sc")
 
